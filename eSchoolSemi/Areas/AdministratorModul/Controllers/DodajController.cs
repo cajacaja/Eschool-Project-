ce15b74b762913bb3b27a18dc43e0ec369a26739
Emir Čajić <emir.cajic@edu.fit.ba>
2018-05-18 22:05:00 +0200

    Npravio Area za administratora,roditelja,nastavnika,i dodo eSchoolData.Moudul u view inmports da mozemo koristit Data klasu u view-ima.

---
+++ eSchoolSemi/Areas/AdministratorModul/Controllers/DodajController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eSchool.Data.Models;
+using eSchoolSemi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eSchoolSemi.Web.Areas.AdministratorModul.Controllers
+{
+
+    [Area("AdministratorModul")]
+    public class DodajController : Controller
+    {
+        private MojContext _context;
+
+        public DodajController(MojContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Grad> model = _context._Grad.ToList();                       
+            return View("Index",model);
+        }
+    }
+}
--- eSchoolSemi/Startup.cs
+++ eSchoolSemi/Startup.cs
@@ -46,6 +46,10 @@ namespace eSchoolSemi
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+             name: "areas",
+             template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+             );
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
